f8aea88f3f1baf328850b44d4fc926dac095e5ad
Robert Wollny <robertjohnwollny@gmail.com>
2019-01-29 02:38:21 -0800

    React Movie Database

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    toggle: true
+  };
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Welcome text="Welcome to Using Props" toggle={this.state.toggle} />
+
+          {this.state.toggle && <p>This should show and hide</p>}
+
+          <button onClick={this.toggle}>Show / Hide</button>
         </header>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text, toggle } = this.props;
+    console.log(toggle);
+    return <h1 className="App-Title">{text}</h1>;
   }
 }
 
